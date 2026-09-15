6e37f7e698c9d90c18fa5c0211cd6794f4670703
Rumpel Stilskin <renegat@renegat0x0.ddns.net>
2026-09-15 16:01:58 +0200

    Fixed entry cleaning

--- db2feeds.py
+++
@@ -1,8 +0,0 @@
-import time
-from sqlalchemy import create_engine
-
-from linkarchivetools.db2feeds import main
-
-
-if __name__ == "__main__":
-    main()
--- linkarchivetools/model/entries.py
+++ linkarchivetools/model/entries.py
@@ -103,18 +103,17 @@ class Entries(BaseTable):
 
     def cleanup(self):
         """
-        Removes invalid entries
+        Cleans up invalid entries.
+        Should not be removed, just disconnected from invalid sources
         """
         ids_to_remove = set()
         for entry in self.connection.entries_table.get_where():
             if entry.source_id is not None:
                 source = self.connection.sources_table.get(id = entry.source_id)
                 if not source:
-                    properties = []
+                    properties = {}
                     properties["source_id"] = None
                     self.connection.entries_table.update_json_data(id=entry.id, json_data=properties)
-
-                    #self.connection.entries_table.delete(id=entry.id)
 
     def delete_invalid_sources(self):
         """
